6d2a314f645411a2d702c0e17504847eea64aa5d
kVarunkk <varunkumawatleap2@gmail.com>
2023-03-07 17:28:43 +0530

    add search feature

---
+++ src/components/Usercard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Usercard(props) {
+  return (
+    <Link to={`/Twitter-Frontend/profile/${props.username}`}>
+      <div className="card">
+        <div className="card-img">
+          <img
+            className="tweet-avatar"
+            src={`https://dark-pink-wasp-boot.cyclic.app/images/${props.avatar}`}
+          ></img>
+        </div>
+        <div className="card-text">
+          <div className="card-text-username">{props.username}</div>
+          <div className="card-text-follow">
+            <div className="card-text-followers">
+              {props.followers.length} followers
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default Usercard;
