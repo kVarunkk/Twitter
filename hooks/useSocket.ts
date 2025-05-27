b8dd6aae97db817c03cab5a4a51e22a2cec5e3ff
kVarunkk <varunkumawatleap2@gmail.com>
2025-05-27 20:50:13 +0530

    fix: socket url env var

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.SOCKET_URL!; // Connect to the separate WebSocket server
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL!; // Connect to the separate WebSocket server
 
 export function useSocket() {
   const [socket, setSocket] = useState<Socket | null>(null);
